a577a4629f35ec4ae1a7fd6702aa2ce7a684a532
Umcaruje <uros.maravic@gmail.com>
2015-01-22 21:32:39 +0100

    Initial Commit

---
+++ js/twinklebox.js
@@ -0,0 +1,33 @@
+/*var twinklebox =	'<div id="twinklebox-overlay">' +
+				'<div id="twinklebox">' +
+				'<img src="' + image_link + '">' +
+				'<div id="twinklebox-nav-left"></div>' +
+				'<div id="twinklebox-nav-right"></div>' +
+				'</div>' +
+				'</div>'; */
+$(document).ready(function () {
+	$("img.twinklebox").each(function () {
+		$(this).wrap('<a href="' + $(this).attr('src') + '" class="twinklebox"></a>');
+		$(this).removeClass("twinklebox");
+	});
+});	
+
+$(document).on('click', '.twinklebox', function (link) {
+	link.preventDefault();
+
+	var image_link = $(this).attr('href');
+
+	var twinklebox = '<div id="twinklebox-overlay">' +
+					'<div id="twinklebox">' +
+					'<img src="' + image_link + '">' +
+					'<div id="twinklebox-nav-left"></div>' +
+					'<div id="twinklebox-nav-right"></div>' +
+					'</div>' +
+					'</div>';
+	$('body').append(twinklebox);
+	$('#twinklebox-overlay').css('opacity', '1');
+});
+
+$(document).on('click', '#twinklebox-overlay', function () {
+	$('#twinklebox-overlay').css('opacity', '0');
+});
