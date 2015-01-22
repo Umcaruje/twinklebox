9b3cb1e1d475eefa78129b784665719590d5c506
Umcaruje <uros.maravic@gmail.com>
2015-01-22 21:41:38 +0100

    Fixya

--- js/twinklebox.js
+++ js/twinklebox.js
@@ -1,33 +1,28 @@
-/*var twinklebox =	'<div id="twinklebox-overlay">' +
-				'<div id="twinklebox">' +
-				'<img src="' + image_link + '">' +
-				'<div id="twinklebox-nav-left"></div>' +
-				'<div id="twinklebox-nav-right"></div>' +
-				'</div>' +
-				'</div>'; */
+/* Twinklebox */
 $(document).ready(function () {
 	$("img.twinklebox").each(function () {
 		$(this).wrap('<a href="' + $(this).attr('src') + '" class="twinklebox"></a>');
 		$(this).removeClass("twinklebox");
 	});
-});	
 
-$(document).on('click', '.twinklebox', function (link) {
-	link.preventDefault();
+	$('.twinklebox').click(function (link) {
+		link.preventDefault();
 
-	var image_link = $(this).attr('href');
+		var image_link = $(this).attr('href');
 
-	var twinklebox = '<div id="twinklebox-overlay">' +
-					'<div id="twinklebox">' +
-					'<img src="' + image_link + '">' +
-					'<div id="twinklebox-nav-left"></div>' +
-					'<div id="twinklebox-nav-right"></div>' +
-					'</div>' +
-					'</div>';
-	$('body').append(twinklebox);
-	$('#twinklebox-overlay').css('opacity', '1');
+		var twinklebox = '<div id="twinklebox-overlay">' +
+						'<div id="twinklebox">' +
+						'<img src="' + image_link + '">' +
+						'<div id="twinklebox-nav-left"></div>' +
+						'<div id="twinklebox-nav-right"></div>' +
+						'</div>' +
+						'</div>';
+		$('body').append(twinklebox);
+		$('#twinklebox-overlay').css('opacity', '1');
+	});
 });
 
 $(document).on('click', '#twinklebox-overlay', function () {
 	$('#twinklebox-overlay').css('opacity', '0');
+	$('#twinklebox-overlay').remove();
 });
