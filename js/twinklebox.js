28fa1422e4d7e2459df1975f069ea74fdcd4bcac
Umcaruje <uros.maravic@gmail.com>
2015-02-01 23:57:55 +0100

    Half baked atm

--- js/twinklebox.js
+++ js/twinklebox.js
@@ -6,42 +6,93 @@ document.addEventListener('DOMContentLoaded', function () {
 	for (var i = 0; i < imgLinks.length; ++i) {
 		imgLinks[i].classList.remove('twinklebox');
 		var currentContent = imgLinks[i].outerHTML;
-		//console.log('classes left are' + imgLinks[i].classList);
 		var wrappedContent = '<a href="' + imgLinks[i].src + '" class="twinklebox">' + currentContent + '</a>';
 		imgLinks[i].outerHTML = wrappedContent;
 	};
+	// Push all links to an array
+	var tbLinks = document.querySelectorAll('a.twinklebox');
+	var tbImages = new Array;
+	for (var i = 0; i < tbLinks.length; i++) {
+		tbImages.push(tbLinks[i].getAttribute('href'))
+	};
 
-	var Tblinks = document.querySelectorAll('a.twinklebox');
+	// Make the tbImages array global
+	window.tbImages = tbImages;
 
-	for (var i = 0; i < Tblinks.length; i++) {
-		Tblinks[i].addEventListener('click', function (link) {
+	for (var i = 0; i < tbLinks.length; i++) {
+		tbLinks[i].addEventListener('click', function (link) {
 			link.preventDefault();
 			// Get the image URL
-			var image_link = this.getAttribute('href');
+			var imageNum = tbImages.indexOf(this.getAttribute('href'));
 
-			//console.log('The image link is ' + image_link)
-
-				// Twinklebox
+			// Twinklebox
 			var twinklebox = '<div id="twinklebox">' +
-				'<img src="' + image_link + '">' +
-				'<div id="tb-nav-left"><svg class="tbi-previous"><use xlink:href="img/sprite.svg#tbi-previous"></use></svg></div>' +
-				'<div id="tb-nav-right"><svg class="tbi-next"><use xlink:href="img/sprite.svg#tbi-next"></use></svg></div>' +
+				'<img src="' + tbImages[imageNum] + '" class="tbImage">' +
+				'<div id="tbNavPrev"><svg class="tbi-previous"><use xlink:href="img/sprite.svg#tbi-previous"></use></svg></div>' +
+				'<div id="tbNavNext"><svg class="tbi-next"><use xlink:href="img/sprite.svg#tbi-next"></use></svg></div>' +
 				'</div>';
+
 			// Create the overlay div
 			var elemDiv = document.createElement('div');
-			elemDiv.id = 'tb-overlay'
+			elemDiv.id = 'tbOverlay'
 			elemDiv.innerHTML = twinklebox;
-			// Add eventListener for closing
-			elemDiv.addEventListener('click', closeTwinklebox, false);
+
 			// Append twinklebox to body
 			document.body.appendChild(elemDiv);
 			elemDiv.classList.add('tbVisible');
-			//$('#tb-overlay').fadeIn(500);
+
+			// Add eventListener for closing
+			elemDiv.addEventListener('click', closeTwinklebox, false);
+
+			// Event handler for the tbNavPrev button
+			elemDiv.querySelector('div#tbNavPrev').addEventListener('click', function (e) {
+				if (!e) var e = window.event;
+				e.cancelBubble = true;
+				if (e.stopPropagation) e.stopPropagation();
+				prevImage(elemDiv);
+			}, false);
+
+			// Event handler for the tbNavNext button
+			elemDiv.querySelector('div#tbNavNext').addEventListener('click', function (e) {
+				if (!e) var e = window.event;
+				e.cancelBubble = true;
+				if (e.stopPropagation) e.stopPropagation();
+				nextImage(elemDiv);
+			}, false);
+
+			elemDiv.querySelector('.tbImage').addEventListener('change', hideNavButtons(), false)
 		});
 	}
 });
 
+var prevImage = function (elemDiv) {
+	var currImg = elemDiv.querySelector('.tbImage').src;
+	console.log(currImg);
+	prev = tbImages.indexOf(currImg) - 1;
+	elemDiv.querySelector('.tbImage').src = tbImages[prev];
+}
+
+var nextImage = function (elemDiv) {
+	var currImg = elemDiv.querySelector('.tbImage').src;
+	next = tbImages.indexOf(currImg) + 1;
+	elemDiv.querySelector('.tbImage').src = tbImages[next];
+}
+
+var hideNavButtons = function (elemDiv) {
+	var currImg = elemDiv.querySelector('.tbImage').src;
+
+	if (tbImages.indexOf(currImg) == 0) {
+		elemDiv.querySelector('#tbNavPrev').style.display = ('none');
+	} else {
+		elemDiv.querySelector('#tbNavPrev').style.display = ('inline-block');
+	};
+	if (tbImages.indexOf(currImg) == tbImages.length) {
+		elemDiv.querySelector('#tbNavNext').style.display = ('none');
+	} else {
+		elemDiv.querySelector('#tbNavNext').style.display = ('inline-block');
+	}
+}
 var closeTwinklebox = function () {
-	var tbOverlay = document.querySelector('#tb-overlay');
+	var tbOverlay = document.querySelector('#tbOverlay');
 	tbOverlay.parentNode.removeChild(tbOverlay);
 };
